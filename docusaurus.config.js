afdff1afd0af6c586f119ec6c9316b9716f5ba8c
JakeLin1023 <105701320+JakeLin1023@users.noreply.github.com>
2023-12-25 13:17:46 +0800

    fix: Docusauru url (#5)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "zkBNB - Official Documentation",
   tagline: "A trustless and scaling solution for BNB Smart Chain",
-  url: "https://bnb-chain.github.io/",
+  url: "https://docs.bnbchain.org",
   baseUrl: "/zkBNB-docs/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
@@ -61,24 +61,24 @@ const config = {
       algolia: {
         // The application ID provided by Algolia
         appId: '3LF005YNGZ',
-  
+
         // Public API key: it is safe to commit it
         apiKey: 'dbc11ec6638f9c767ef6ed2856871f58',
-  
+
         indexName: 'bnbchain',
-  
+
         // Optional: see doc section below
         // contextualSearch: true,
-  
+
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         externalUrlRegex: 'https://docs.bnbchain.org/',
-  
+
         // Optional: Algolia search parameters
         //searchParameters: {},
-  
+
         // Optional: path for search page that enabled by default (`false` to disable it)
-      //  searchPagePath: 'search',
-  
+        //  searchPagePath: 'search',
+
         //... other Algolia params
       },
       navbar: {
@@ -115,7 +115,7 @@ const config = {
           },
         ]
       },
-      
+
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
